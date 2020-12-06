2cb75c1f0f492c1be3c7427f48bb00bb5d0bebe7
Ametrin <878027460@qq.com>
2020-12-06 16:45:47 +0800

    spring 初始

---
+++ src/test/java/com/ame/course/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ame.course.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
