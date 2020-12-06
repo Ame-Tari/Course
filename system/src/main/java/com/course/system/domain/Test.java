421cbd7ef2faa4748296c3b97771df5627ae4cb7
Ametrin <878027460@qq.com>
2020-12-06 19:31:40 +0800

    1.mapper简单配置(2)

---
+++ system/src/main/java/com/course/system/domain/Test.java
@@ -0,0 +1,15 @@
+package com.course.system.domain;
+
+import lombok.Data;
+
+/**
+ * @author huangmingtao
+ */
+
+@Data
+public class Test {
+
+    private String id;
+
+    private String name;
+}
---
+++ system/src/main/java/com/course/system/mapper/TestMapper.java
@@ -0,0 +1,14 @@
+package com.course.system.mapper;
+
+import com.course.system.domain.Test;
+
+import java.util.List;
+
+/**
+ * @author huangmingtao
+ */
+public interface TestMapper {
+
+    public List<Test> list();
+
+}
