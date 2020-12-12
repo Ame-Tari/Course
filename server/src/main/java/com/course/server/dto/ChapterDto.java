9e9e083ccacc891ec65a0234681b7f9288af0a82
ming <878027460@qq.com>
2020-12-12 15:25:14 +0800

    1.大页面显示真实数据

--- server/src/main/java/com/course/server/dto/ChapterDto.java
+++ server/src/main/java/com/course/server/dto/ChapterDto.java
@@ -1,9 +1,20 @@
 package com.course.server.dto;
 
+import lombok.Data;
+import lombok.NonNull;
+
 /**
  * @author huangmingtao
  */
+@Data
 public class ChapterDto {
 
+    private String id;
+
+    @NonNull
+    private String courseId;
+
+    @NonNull
+    private String name;
 
 }
