165d376076c082b98d611ca92dec335fa7088b8b
ming <878027460@qq.com>
2020-12-14 13:54:06 +0800

    1.前端增加日志

--- admin/src/main.js
+++ admin/src/main.js
@@ -4,6 +4,20 @@ import router from './router'
 import axios from "axios";
 Vue.config.productionTip = false;
 Vue.prototype.$ajax = axios;
+
+axios.interceptors.request.use(function (config) {
+    console.log("请求：", config);
+    return config;
+  }, error => {
+
+  });
+axios.interceptors.request.use(function (response) {
+  console.log("返回结果", response);
+  return response;
+}, error => {
+
+});
+
 new Vue({
   router,
   render: h => h(App),
